6d2350137c1e810f35d313f160bc47bc0d339388
Marco <marcobaruco2091@gmail.com>
2025-01-07 08:06:42 -0300

    fim

--- script/script.js
+++ script/script.js
@@ -1,3 +1,12 @@
+AOS.init();
+
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function() {
+        $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-bars fa-x');
+    });
+});
+
 let count = 1;
 document.getElementById('radio1').checked = true;
 
